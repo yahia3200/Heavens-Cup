f3454dbda2f3181188b22f275a0ea96ea656277f
hoskillua <47090776+hoskillua@users.noreply.github.com>
2022-12-25 18:40:16 +0200

    some progress

--- WC22Front/src/Components/MatchPage/chars.tsx
+++ WC22Front/src/Components/MatchPage/chars.tsx
@@ -50,4 +50,38 @@ export const chars = [
     "Gotoh",
 ]
 
+interface CharData {
+    name: string;
+    image: string;
+    "hue-rotate": string;
+}
+
+export const charsData = new Map<string, CharData>([
+    ["Gon", {
+        name: "Gon Freecss",
+        image: "/src/assets/Chars/Gon.png",
+        "hue-rotate": "180deg",
+    }],
+    ["Killua", {
+        name: "Killua Zoldyck",
+        image: "/src/assets/Chars/Killua.png",
+        "hue-rotate": "320deg",
+    }],
+    ["Kurapika", {
+        name: "Kurapika Kurta",
+        image: "/src/assets/Chars/Kurapika.png",
+        "hue-rotate": "80deg",
+    }],
+    ["Leorio", {
+        name: "Leorio Paradinight",
+        image: "/src/assets/Chars/Leorio.png",
+        "hue-rotate": "0deg",
+    }],
+    ["Hisoka", {
+        name: "Hisoka Morow",
+        image: "/src/assets/Chars/Hisoka.png",
+        "hue-rotate": "40deg",
+    }],
+])
+
 // length of chars array = 39
--- WC22Front/src/pages/MatchPage.tsx
+++ WC22Front/src/pages/MatchPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "../styles/MatchPage.scss";
+import { charsData } from "../Components/MatchPage/chars";
 
 export default function MatchPage() {
+  const match = {
+    date: "11 - 06 - 2022",
+    time: "10:30 GMT",
+    team1: "Hisoka",
+    team2: "Kurapika",
+    refrees: ["Keenan Crane", "Yalla Negro", "Amin Elhassan"],
+    stadium: "awgwa",
+  };
+
   return (
     <div className="match-page">
       <div className="match-page__overlay-image">
@@ -9,23 +19,30 @@ export default function MatchPage() {
       <div className="match-page__overlay">
       </div>
       <div className="match-page__match">
-        <div className="match-page__match__date">2021-06-11</div>
+        <div className="match-page__match__date-and-time-container">
+          <div className="match-page__match__date-and-time-container__date">{match.date}</div>
+          <div className="match-page__match__date-and-time-container__time">{match.time}</div>
+        </div>
         <div className="match-page__match__teams">
-          <div className="match-page__match__teams__team">
-            <div className="match-page__match__teams__team__flag">🏴󠁧󠁢󠁥󠁮󠁧󠁿</div>
-            <div className="match-page__match__teams__team__name">England</div>
+          <div className="match-page__match__teams__team" style={{filter: `hue-rotate(${charsData.get(match.team1)!["hue-rotate"]})`}}>
+            <div className="match-page__match__teams__team__flag">
+              <img src={ charsData.get(match.team1)!.image } className="match-page__match__teams__team__flag__img"  style={{filter: `hue-rotate(-${charsData.get(match.team1)!["hue-rotate"]})`}} />
+            </div>
+            <div className="match-page__match__teams__team__name match-page__match__teams__team__name--team-1">{charsData.get(match.team1)!.name}</div>
           </div>
-          <div className="match-page__match__teams__team">
-            <div className="match-page__match__teams__team__flag">🇫🇷</div>
-            <div className="match-page__match__teams__team__name">France</div>
+          <div className="match-page__match__teams__team" style={{filter: `hue-rotate(${charsData.get(match.team2)!["hue-rotate"]})`}}>
+            <div className="match-page__match__teams__team__name match-page__match__teams__team__name--team-2">{charsData.get(match.team2)!.name}</div>
+            <div className="match-page__match__teams__team__flag">
+              <img src={charsData.get(match.team2)!.image} className="match-page__match__teams__team__flag__img"  style={{filter: `hue-rotate(-${charsData.get(match.team2)!["hue-rotate"]})`}} />
+            </div>
           </div>
         </div>
         <div className="match-page__match__refrees">
-            <div className="match-page__match__refrees__primary-refree"> Hamada </div>
-            <div className="match-page__match__refrees__secondary-refree"> Alaa </div>
-            <div className="match-page__match__refrees__secondary-refree"> Alaa </div>
+            <div className="match-page__match__refrees__header"> Refrees </div>
+            <div className="match-page__match__refrees__primary-refree"> {match.refrees[0]} </div>
+            <div className="match-page__match__refrees__secondary-refree"> {match.refrees[1]} </div>
+            <div className="match-page__match__refrees__secondary-refree"> {match.refrees[2]} </div>
         </div>
-        <div className="match-page__match__stadium">awgwa</div>
       </div>
     </div>
   );
