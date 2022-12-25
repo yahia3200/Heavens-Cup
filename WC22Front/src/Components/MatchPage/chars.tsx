674c9acd8c3e0ab7dd9b77385b1f99f161c3e5f6
hoskillua <47090776+hoskillua@users.noreply.github.com>
2022-12-25 13:21:06 +0200

    chars

---
+++ WC22Front/src/Components/MatchPage/chars.tsx
@@ -0,0 +1,53 @@
+export const chars = [
+    "Gon Freecss",
+    "Killua Zoldyck",
+    "Kurapika",
+    "Leorio Paradinight",
+
+    "Isaac Netero",
+    "Pariston Hill",
+    "Ging Freecss",
+    "Kite",
+
+    "Morel",
+    "Knov",
+    "Knuckle Bine",
+    "Shoot",
+
+    "Biscuit Krueger",
+    "Hisoka Morow",
+
+    "Wing",
+    "Zushi",
+
+    "Pitou",
+    "Meruem",
+    "Pouf",
+    "Youpi",
+
+    "Ikalgo",
+    "Meleron",
+
+    "Chrollo Lucilfer",
+    "Shalnark",
+    "Shizuku",
+    "Feitan",
+    "Phinks",
+    "Bonolenov",
+    "Uvogin",
+    "Nobunaga Hazama",
+    "Machi Komacine",
+    "Franklin",
+
+    "Genthru",
+    "Razor",
+
+    "Illumi Zoldyck",
+    "Zeno Zoldyck",
+    "Kalluto Zoldyck",
+    "Silva Zoldyck",
+
+    "Gotoh",
+]
+
+// length of chars array = 39
