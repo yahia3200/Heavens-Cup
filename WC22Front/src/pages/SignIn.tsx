ce0214ce384f1ffa386e27086e7a292ce4ac0b2e
Yahia Zakaria <yahiazakaria3200@gmail.com>
2022-12-25 13:40:22 +0200

    add sass suport

---
+++ WC22Front/src/pages/SignIn.tsx
@@ -0,0 +1,13 @@
+interface SignINProps {
+
+}
+
+const SignIN: React.FunctionComponent<SignINProps> = () => {
+    return (
+        <div>
+            <h1>Sign In</h1>
+        </div>
+    );
+}
+
+export default SignIN;
