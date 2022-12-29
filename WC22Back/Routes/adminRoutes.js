950e954d628c778ca366fa9192d6fe6c17b1648d
Ziad <ziadatef35@hotmail.com>
2022-12-29 17:01:05 +0200

    DB updated

---
+++ WC22Back/Routes/adminRoutes.js
@@ -0,0 +1,8 @@
+const Router = require('express');
+const adminController = require('../Controller/adminController');
+
+const router = Router();
+
+
+
+module.exports = router;
--- WC22Back/Routes/guestRoutes.js
+++ WC22Back/Routes/guestRoutes.js
@@ -3,7 +3,7 @@ const guestController = require('../Controller/guestController');
 
 const router = Router();
 
-//router.get('/view_match_details', guestController.view_match_details);
+router.get('/view_match_details', guestController.view_match_details);
 
 router.get('/get_all_stadiums', guestController.get_all_stadiums);
 
