0874ad6eeb2bc7b5594a2885d18a957aa621bd8f
osamamagdy <osamamagdy174@gmail.com>
2022-12-30 00:40:46 +0200

    add token in request
    Signed-off-by: osamamagdy <osamamagdy174@gmail.com>

--- WC22Front/src/Components/CreateMatch.tsx
+++ WC22Front/src/Components/CreateMatch.tsx
@@ -101,6 +101,7 @@ const CreateMatch: React.FunctionComponent<CreateMatchProps> = ({ open, setOpen 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`
             }
         })
             .then(res => res.json())
@@ -128,6 +129,7 @@ const CreateMatch: React.FunctionComponent<CreateMatchProps> = ({ open, setOpen 
         const firstLinesmenId = availableReferees.find(referee => referee.name === firstLinesmen)?.id;
         const secondLinesmenId = availableReferees.find(referee => referee.name === secondLinesmen)?.id;
 
+        console.log(firstTeam, secondTeam, arena, referee, firstLinesmen, secondLinesmen, date)
         console.log(firstTeamId, secondTeamId, arenaId, refereeId, firstLinesmenId, secondLinesmenId, date)
     }
 
