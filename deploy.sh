f2ac1c7da55d5dae3bca73edcf87413e058ca358
osamamagdy <osamamagdy174@gmail.com>
2022-12-29 15:24:42 +0200

    edit script
    Signed-off-by: osamamagdy <osamamagdy174@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
 docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -v postgres-data:/var/lib/postgresql/data -d postgres
+
+docker build --tag osamamagdy/wc22_backend:latest .
+
+docker push osamamagdy/wc22_backend
+
