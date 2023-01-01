0b9a12e91994a7750b735746f9ccb8904134e569
osamamagdy <osamamagdy174@gmail.com>
2023-01-01 16:36:07 +0200

    edit match
    Signed-off-by: osamamagdy <osamamagdy174@gmail.com>

--- WC22Back/Controller/managerController.js
+++ WC22Back/Controller/managerController.js
@@ -12,32 +12,33 @@ module.exports = {
             const matches = await matchconnection.getMatches();
             // Check if matches are not zero
             if(matches.length != 0) {
-            
-            matches.forEach(match => {
-                if((match.team1 == req.body.team1 || match.team2 == req.body.team1) ||
-                 (match.team1 == req.body.team2 || match.team2 == req.body.team2) ||
-                 (match.main_ref == req.body.main_ref) || (match.line_man_1 == req.body.main_ref) || (match.line_man_2 == req.body.main_ref) ||
-                 (match.main_ref == req.body.line_man_1) || (match.line_man_1 == req.body.line_man_1) || (match.line_man_2 == req.body.line_man_1) ||
-                 (match.main_ref == req.body.line_man_2) || (match.line_man_1 == req.body.line_man_2) || (match.line_man_2 == req.body.line_man_2) ||
-                 (match.stad_id == req.body.stad_id))
+            for (let i = 0; i < matches.length; i++) {
+                // Check if the teams are not present in a match on the same day
+                if((matches[i].team1 == req.body.team1 || matches[i].team2 == req.body.team1) ||
+                 (matches[i].team1 == req.body.team2 || matches[i].team2 == req.body.team2) ||
+                 (matches[i].main_ref == req.body.main_ref) || (matches[i].line_man_1 == req.body.main_ref) || (matches[i].line_man_2 == req.body.main_ref) ||
+                 (matches[i].main_ref == req.body.line_man_1) || (matches[i].line_man_1 == req.body.line_man_1) || (matches[i].line_man_2 == req.body.line_man_1) ||
+                 (matches[i].main_ref == req.body.line_man_2) || (matches[i].line_man_1 == req.body.line_man_2) || (matches[i].line_man_2 == req.body.line_man_2) ||
+                 (matches[i].stad_id == req.body.stad_id))
                 {
+                    console.log("HELLO555")
+
+                    const matchDate = new Date(matches[i].start_time).toISOString().split('T')[0];
+                    const reqDate = new Date(req.body.start_time).toISOString().split('T')[0];
                     
-                    const match_date = req.body.start_time;
-                    
-                    // Compare the day of the match with the day of the new match
-                    if( new Date(match_date) == new Date(req.body.start_time) ) {
+                    // Compare the day of the matches[i] with the day of the new matches[i]
+                    if( matchDate == reqDate ) {
                         console.log('There is another match on the same day');
-                        res.status(405).json({ error: 'There is another match on the same day'});
+                        res.status(400).json({ error: 'There is another match on the same day'});
                         return;
                     }
                 }  
-            });
+            }
         }
-            console.log('No match on the same day');
             const match = await matchconnection.insertMatch(req.body);
             res.status(200).json({ id: match.id, message: 'Match created successfully' });
         } catch (err) {
-            res.status(400).json({error: err.detail});
+            res.status(400).json({error: "Error creating match"});
         }
     },
     create_stadium: async (req, res) => {
@@ -46,7 +47,7 @@ module.exports = {
             res.status(200).json({stad_id: stad.id});
         } catch (err) {
             console.log(err);
-            res.status(400).json({error: err.detail});
+            res.status(400).json({error: "Error creating stadium"});
         }
     },
     edit_match: async (req, res) => {
@@ -55,30 +56,33 @@ module.exports = {
             const matches = await matchconnection.getMatches();
             // Check if matches are not zero
             if(matches.length != 0) {
-                matches.forEach(match => {
-                    if((match.team1 == req.body.team1 || match.team2 == req.body.team1) ||
-                    (match.team1 == req.body.team2 || match.team2 == req.body.team2) ||
-                    (match.main_ref == req.body.main_ref) || (match.line_man_1 == req.body.main_ref) || (match.line_man_2 == req.body.main_ref) ||
-                    (match.main_ref == req.body.line_man_1) || (match.line_man_1 == req.body.line_man_1) || (match.line_man_2 == req.body.line_man_1) ||
-                    (match.main_ref == req.body.line_man_2) || (match.line_man_1 == req.body.line_man_2) || (match.line_man_2 == req.body.line_man_2) ||
-                    (match.stad_id == req.body.stad_id))
+                for (let i = 0; i < matches.length; i++) {
+                    // Check if the teams are not present in a match on the same day
+                    if((matches[i].team1 == req.body.team1 || matches[i].team2 == req.body.team1) ||
+                     (matches[i].team1 == req.body.team2 || matches[i].team2 == req.body.team2) ||
+                     (matches[i].main_ref == req.body.main_ref) || (matches[i].line_man_1 == req.body.main_ref) || (matches[i].line_man_2 == req.body.main_ref) ||
+                     (matches[i].main_ref == req.body.line_man_1) || (matches[i].line_man_1 == req.body.line_man_1) || (matches[i].line_man_2 == req.body.line_man_1) ||
+                     (matches[i].main_ref == req.body.line_man_2) || (matches[i].line_man_1 == req.body.line_man_2) || (matches[i].line_man_2 == req.body.line_man_2) ||
+                     (matches[i].stad_id == req.body.stad_id))
                     {
+                            
+                        const matchDate = new Date(matches[i].start_time).toISOString().split('T')[0];
+                        const reqDate = new Date(req.body.start_time).toISOString().split('T')[0];
                         
-                        const match_date = req.body.start_time;
-                        
+                            
                         // Compare the day of the match with the day of the new match
-                        if( new Date(match_date) == new Date(req.body.start_time)  && match.id != req.body.id) {
+                        if( matchDate == reqDate  && matches[i].id != req.body.id) {
                             console.log('There is another match on the same day');
                             res.status(405).json({ error: 'There is another match on the same day'});
                             return;
                         }
                     }  
-                });
+                }
             }
-            const match = await matchconnection.updateMatch(req.body.match);
+            const match = await matchconnection.updateMatch(req.body);
             res.status(200).json({id: match.id});
         } catch (err) {
-            res.status(400).json({error: err.detail});
+            res.status(400).json({error: "Error editing match"});
         }
     }
 }
